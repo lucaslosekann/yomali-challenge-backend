da19e282fdb65c63353cb632971f672d9b2c49d5
Lucas Losekann <lucas1losekann@gmail.com>
2025-08-24 23:17:11 -0300

    refactor: simplify tracking controller by removing JSON parsing and validation logic

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as express from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -13,8 +12,7 @@ async function bootstrap() {
     const documentFactory = () => SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, documentFactory);
 
-    app.use(express.text({ type: 'text/plain' }));
-    app.enableCors();
+    app.enableCors({ credentials: true, origin: true });
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(process.env.PORT ?? 3000);
 }
